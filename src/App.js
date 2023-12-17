b229c9e19bcc3cfa7bcf0c563dc54cb499e72679
Ayanthika Subasinghe <saasubasinghe@std.appsc.sab.ac.lk>
2023-12-18 06:41:29 +0700

    create messages list page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import UserPatient from "./Pages/Patientpage/UserPatient";
 import UserDoctor from "./Pages/doctorpage/UserDoctor";
 import UserPharmacist from "./Pages/pharmacistpage/UserPharmacist";
+import Messages from "./Pages/messagepage/Messages";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <UserPatient/>
       <UserPharmacist/>
       <UserDoctor/>
+      <Messages/>
     </div>
   );
 }
